724f7301845b5edc10a3b5348a4767c95b5cdfff
Diego Gomes <diegofgomes19@gmail.com>
2021-02-28 16:29:04 -0300

    Ajustes no posicionamento dos atalhos do menu de navegacao

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 (function($){
 	'use strict';
 	$(document).ready(function() {
+		/*--------- atalhos para as seções em tela Desktop  ------------*/
 		$('.active').click(function(){
 			$('html, body').animate({scrollTop: 900}, 600);
 		});
@@ -13,9 +14,33 @@
 		$('.nav_rodape').click(function(){
 			$('html, body').animate({scrollTop: 4500}, 600);
 		});
+		
+		/*--------- atalhos para as seções em tela até 600px ------------*/
+
+		function mediaQuerie(x){
+	
+				$('.active').click(function(){
+					$('html, body').animate({scrollTop: 330}, 600);
+				});
+				$('.digital').click(function(){
+					$('html, body').animate({scrollTop: 1000}, 600);
+				});
+				$('.servicos').click(function(){
+					$('html, body').animate({scrollTop: 2500}, 600);
+				});
+				$('.nav_rodape').click(function(){
+					$('html, body').animate({scrollTop: 5000}, 600);
+				});
+		}
+
+		var x = window.matchMedia('(max-width: 600px)');
+		mediaQuerie();
+		x.addListener(mediaQuerie);
+
 		$('.navegacao-itens-lista').click(function(){
 			$('.checkbtn').click();
 			
 		});
+
 	});
 }(jQuery));
