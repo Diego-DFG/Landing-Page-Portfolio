c794e530fa77fc680ac10d35011d09f382f61814
Diego Gomes <diegofgomes19@gmail.com>
2021-03-01 16:46:47 -0300

    Refatoracao do codigo de custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,46 +1,83 @@
 (function($){
 	'use strict';
 	$(document).ready(function() {
-		/*--------- atalhos para as seções em tela Desktop  ------------*/
-		$('.active').click(function(){
-			$('html, body').animate({scrollTop: 900}, 600);
-		});
-		$('.digital').click(function(){
-			$('html, body').animate({scrollTop: 1800}, 600);
-		});
-		$('.servicos').click(function(){
-			$('html, body').animate({scrollTop: 2300}, 600);
-		});
-		$('.nav_rodape').click(function(){
-			$('html, body').animate({scrollTop: 4500}, 600);
-		});
 		
-		/*--------- atalhos para as seções em tela até 600px ------------*/
+		/*--------- Variáveis até 600px ------------*/
+		let missaoTelaMedia = $('.active')
+						.click(function(){$('html, body')
+						.animate({scrollTop: 330}, 600);});
+		let digitalTelaMedia = $('.digital')
+						.click(function(){$('html, body')
+						.animate({scrollTop: 1000}, 600);});
+		let servicosTelaMedia = $('.servicos')
+						.click(function(){$('html, body')
+						.animate({scrollTop: 2500}, 600);});
+		let rodapeTelaMedia = $('.nav_rodape')
+						.click(function(){$('html, body')
+						.animate({scrollTop: 5000}, 600);});
+		/*------------------------------------------------------------------*/
 
-		function mediaQuerie(x){
-	
-				$('.active').click(function(){
-					$('html, body').animate({scrollTop: 330}, 600);
-				});
-				$('.digital').click(function(){
-					$('html, body').animate({scrollTop: 1000}, 600);
-				});
-				$('.servicos').click(function(){
-					$('html, body').animate({scrollTop: 2500}, 600);
-				});
-				$('.nav_rodape').click(function(){
-					$('html, body').animate({scrollTop: 5000}, 600);
-				});
+
+		/*----------- Variáveis para desktop --------*/
+		let missaoTelaDesktop = $('.active')
+						.click(function(){$('html, body')
+						.animate({scrollTop: 900}, 600);});
+		let digitalTelaDesktop = $('.digital')
+						.click(function(){$('html, body')
+						.animate({scrollTop: 1800}, 600);});
+		let servicosTelaDesktop = $('.servicos')
+						.click(function(){$('html, body')
+						.animate({scrollTop: 2300}, 600);});
+		let rodapeTelaDesktop = $('.nav_rodape')
+						.click(function(){$('html, body')
+						.animate({scrollTop: 5000}, 600);});
+		/*------------------------------------------------------------------*/
+
+
+
+		/*------------ Variável de click botão check --------*/
+		let clicaBotaoCheck = $('.navegacao-itens-lista')
+								.click(function(){$('.checkbtn').click();});
+		/*------------------------------------------------------------------*/
+
+
+
+		/*------------ Variáveis da função -----------*/
+		var x = window.matchMedia('(max-width: 600px)');
+		var y = window.matchMedia('(min-width: 1100px)');
+		/*------------------------------------------------------------------*/
+
+
+		/* ------------- Função -----------------------*/
+
+		function mediaQuerieTelaMedia(x){
+			if(x.Matches) {
+				missaoTelaMedia;
+				digitalTelaMedia;
+				servicosTelaMedia;
+				rodapeTelaMedia;
+			}
 		}
 
-		var x = window.matchMedia('(max-width: 600px)');
-		mediaQuerie();
-		x.addListener(mediaQuerie);
+		function mediaQuerieTelaDesktop(y){
+			if(y.Matches) {
+				missaoTelaDesktop;
+				digitalTelaDesktop;
+				servicosTelaDesktop;
+				rodapeTelaDesktop;
+			}
+		}
 
-		$('.navegacao-itens-lista').click(function(){
-			$('.checkbtn').click();
-			
-		});
+
+
+		mediaQuerieTelaMedia(x);
+		x.addListener(mediaQuerieTelaMedia);
+
+		mediaQuerieTelaDesktop(y);
+		y.addListener(mediaQuerieTelaDesktop);
+
+		
+		clicaBotaoCheck;
 
 	});
 }(jQuery));
